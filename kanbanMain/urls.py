a5321a7a170b48137dc6bc871bbc67951be11244
Austin <austin.fratesi@gmail.com>
2025-04-22 22:28:14 -0500

    updates 4-22-25

--- kanbanMain/urls.py
+++ kanbanMain/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('', views.mainKanbanPage),
   path('home', views.mainKanbanPage),
+  path('kanbanBoard', views.mainKanbanPage),
   path('addNewTask', views.addNewTask)
 ]
